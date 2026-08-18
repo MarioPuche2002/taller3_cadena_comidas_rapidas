5a85d4795858b3e209cc4a95665d957f9f76fedd
Mario Puche <marioandres200271@hotmail.com>
2026-08-17 22:38:01 -0500

    Incluimos metricas validacion y entrenamiento para evaluar sobreajustes

--- src/data.py
+++ src/data.py
@@ -6,7 +6,7 @@ def cargar_datos():
     par = parametros()
     file_path = par["ruta_archivo"]
     df = pd.read_csv(file_path, parse_dates=["fecha"])
+    df = df.sort_values("fecha").reset_index(drop=True)
     print(df)
 
     return df
-cargar_datos()
--- src/features.py
+++ src/features.py
@@ -2,9 +2,9 @@ from data import cargar_datos
 
 def data_split():
     df = cargar_datos()
-    df = df.sort_values("fecha").reset_index(drop=True)
     
-    X = df[["dia_semana", "temperatura_c", "llovio", "precio", "es_quincena"]]
+    df['mes'] = df['fecha'].dt.month
+    X = df[["dia_semana", "mes", "temperatura_c", "precio", "es_quincena"]]
     y = df["almuerzos"]
     
     return X, y
