cd521a7a744b585a4954c1bd1ed31f8c64ac886b
Mario Puche <marioandres200271@hotmail.com>
2026-08-17 22:52:12 -0500

    Onehot para mes y dia seamana

--- src/data.py
+++ src/data.py
@@ -7,6 +7,17 @@ def cargar_datos():
     file_path = par["ruta_archivo"]
     df = pd.read_csv(file_path, parse_dates=["fecha"])
     df = df.sort_values("fecha").reset_index(drop=True)
+    
+    df['almuerzos_ayer'] = df['almuerzos'].shift(1).bfill()
+    df['almuerzos_semana_pasada'] = df['almuerzos'].shift(7).bfill()
+    
+    df_dias = pd.get_dummies(df['dia_semana'], prefix='dia', dtype=int)
+    df = pd.concat([df, df_dias], axis=1)
+    
+    df['mes'] = df['fecha'].dt.month
+    df_meses = pd.get_dummies(df['mes'], prefix='mes', dtype=int)
+    df = pd.concat([df, df_meses], axis=1)
+
     print(df)
 
     return df
--- src/features.py
+++ src/features.py
@@ -1,21 +1,12 @@
 from data import cargar_datos
 
 def data_split():
-    df = cargar_datos().copy()
-    df = df.sort_values('fecha')
+    df = cargar_datos()
     
-    df['almuerzos_ayer'] = df['almuerzos'].shift(1)
-    df['almuerzos_semana_pasada'] = df['almuerzos'].shift(7)
+    columnas_dias = [col for col in df.columns if col.startswith('dia_')]
+    columnas_meses = [col for col in df.columns if col.startswith('mes_')]
     
-    df['almuerzos_ayer'] = df['almuerzos_ayer'].bfill()
-    df['almuerzos_semana_pasada'] = df['almuerzos_semana_pasada'].bfill()
-    
-    df['mes'] = df['fecha'].dt.month
-    
-    columnas_features = [
-        "dia_semana", "mes", "temperatura_c", "llovio", 
-        "precio", "es_quincena", "almuerzos_ayer", "almuerzos_semana_pasada"
-    ]
+    columnas_features = ["temperatura_c", "precio", "es_quincena", "almuerzos_ayer", "almuerzos_semana_pasada"] + columnas_dias + columnas_meses
     
     X = df[columnas_features]
     y = df["almuerzos"]
