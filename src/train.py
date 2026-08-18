abc56599506812e54fffe934539cbab03a3c6879
Mario Puche <marioandres200271@hotmail.com>
2026-08-17 22:27:50 -0500

    Pruebas_sobreajuste_regresionlineal

--- src/train.py
+++ src/train.py
@@ -1,9 +1,10 @@
 from features import data_split
 from model import crear_modelo
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, r2_score
 
 def training():
-
+    
     X, y = data_split()
     train_size = int(len(X) * 0.8)
     
@@ -16,7 +17,15 @@ def training():
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train) 
     X_test_scaled = scaler.transform(X_test)       
+    
     modelo = crear_modelo()
     modelo.fit(X_train_scaled, y_train)
     
-    return modelo, X_test_scaled, y_test, scaler
+    pred_train = modelo.predict(X_train_scaled)
+    mae_train = mean_absolute_error(y_train, pred_train)
+    mape_train = mean_absolute_percentage_error(y_train, pred_train) * 100
+    r2_train = r2_score(y_train, pred_train)
+    
+    metricas_train = {"mae": mae_train,"mape": mape_train,"r2": r2_train}
+    
+    return modelo, X_test_scaled, y_test, scaler, metricas_train
--- src/validar.py
+++ src/validar.py
@@ -1,19 +1,23 @@
 from train import training
-from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, r2_score
 
 def validar():
+    modelo, X_test_scaled, y_test, scaler, metricas_train = training()
+    pred_test = modelo.predict(X_test_scaled)
 
-    modelo, X_test_scaled, y_test, scaler = training()
-    pred = modelo.predict(X_test_scaled)
-
-    mae = mean_absolute_error(y_test, pred)
-    mape = mean_absolute_percentage_error(y_test, pred) * 100
+    mae_test = mean_absolute_error(y_test, pred_test)
+    mape_test = mean_absolute_percentage_error(y_test, pred_test) * 100
+    r2_test = r2_score(y_test, pred_test)
     
-    print("=== AUDITORÍA / MAE HONESTO ===")
-    print(f"MAE :",mae,"almuerzos")
-    print(f"MAPE:",mape,"%")
+    print("=== AUDITORÍA / COMPARATIVA DE MODELO ===")
+    print(f"Métrica       | Entrenamiento | Prueba (Test)")
+    print(f"---------------------------------------------")
+    print(f"MAE           | {metricas_train['mae']:.2f}         | {mae_test:.2f} almuerzos")
+    print(f"MAPE          | {metricas_train['mape']:.2f}%        | {mape_test:.2f}%")
+    print(f"R2            | {metricas_train['r2']:.2f}          | {r2_test:.2f}")
+    print("=============================================")
     
-    return mae, mape
+    return mae_test, mape_test
 
 if __name__ == "__main__":
     validar()
