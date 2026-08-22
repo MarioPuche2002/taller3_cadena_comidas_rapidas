3f0054eb0b0dedf8a42e05b4790059ece23d283a
Mario Puche <marioandres200271@hotmail.com>
2026-08-22 03:27:45 -0500

    model reestructurado

--- src/config.py
+++ src/config.py
@@ -1,25 +1,8 @@
+from pathlib import Path
 import os
 
 
 def parametros():
     ruta_archivo = os.path.join(os.getcwd(), "data", "almuerzos_entrenamiento.csv")
-
-    par = {
-        "random_state": 42,
-        "ruta_archivo": ruta_archivo,
-        "columna_fecha": "fecha",
-        "columna_objetivo": "almuerzos",
-
-        "test_size": 0.2,   
-
-        "features_numericas": ["temperatura_c", "precio", "dias_desde_inicio"],
-        "features_categoricas": ["dia_semana", "llovio", "es_quincena"],
-
-        "estrategia_imputacion_numerica": "median",
-        "estrategia_imputacion_categorica": "most_frequent",
-
-        "alpha": 0.1,
-        "l1_ratio": 0.5,
-        "max_iter": 30000,}
-    
+    par = {"random_state": 42,"test_size": 0.2,"ruta_archivo": ruta_archivo,"alpha": 0.1,"l1_ratio": 0.5,"max_iter": 30000}
     return par
--- src/model.py
+++ src/model.py
@@ -1,16 +1,17 @@
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import ElasticNet
+from config import parametros
 
 def crear_modelo():
+    par = parametros()
 
-    modelo = Pipeline([
-        ('scaler', StandardScaler()),
-        ('elastic', ElasticNet(
-            alpha=0.1,
-            l1_ratio=0.5,
-            random_state=42,
-            max_iter=30000
-        ))])
+    alpha_value = par["alpha"]
+    l1_ratio_value = par["l1_ratio"]
+    max_iter_value = par["max_iter"]
+    random_state_value = par["random_state"]
+
+
+    modelo = Pipeline([('scaler', StandardScaler()),('elastic', ElasticNet(alpha=alpha_value,l1_ratio=l1_ratio_value,random_state=random_state_value,max_iter=max_iter_value))])
     
     return modelo
